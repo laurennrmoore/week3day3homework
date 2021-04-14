67b27038000068d34e4f9d633c24420a26825735
laurennrmoore <lmooreintech2021@gmail.com>
2021-04-14 18:41:28 -0400

    day3 wk 33winitialization

---
+++ day3wk3.js
@@ -0,0 +1,38 @@
+ var a=1;
+ var b=2;
+ var c =3;
+ var d = 4;
+ var e = 5;
+ var f = 6;
+ var g =7;
+ var h =8;
+var i = 9;
+var j = 10;
+
+
+
+console.log(a + b);
+
+function subtractTwo(j,h){
+answer(j-h);
+console.log;    
+}
+
+function subtractTwo(i,f){
+answer(i-f);
+console.log;
+}
+
+function multiplyTwo(e,d){
+answer(e*d);
+console.log;    
+}
+
+function multiplyTwo(a,j){
+answer(a*j);
+console.log;
+}
+
+function divideTwo(j,b){
+
+}
